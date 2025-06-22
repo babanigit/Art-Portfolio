4f5349a2d85fd5c2e33d9abb1e6fec170c141916
aniket panchal <aniketpanchal07@gmail.com>
2025-06-22 12:19:49 +0530

    first commit , created the basics nextjs app with tailwind css, added navbar and footer , exited to build the rest of the components

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    (
+        <>
+        
+        <div>
+            Hello Footer
+        </div>
+        
+        </>
+    )
+  )
+}
+
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+/*
+home
+experience
+skills
+education
+projects
+connect
+*/
+
+const Header = () => {
+  return (
+    <ul className="flex space-x-6 p-4">
+      <li>
+        <a href="#home" className="hover:text-blue-600">
+          home
+        </a>
+      </li>
+      <li>
+        <a href="#experience" className="hover:text-blue-600">
+          experience
+        </a>
+      </li>
+      <li>
+        <a href="#skills" className="hover:text-blue-600">
+          skills
+        </a>
+      </li>
+      <li>
+        <a href="#education" className="hover:text-blue-600">
+          education
+        </a>
+      </li>
+      <li>
+        <a href="#projects" className="hover:text-blue-600">
+          projects
+        </a>
+      </li>
+      <li>
+        <a href="#connect" className="hover:text-blue-600">
+          connect
+        </a>
+      </li>
+      <li>
+        <a href="/resume" className="hover:text-blue-600">
+          resume
+        </a>
+      </li>
+      <li>
+        <a href="/portfolio" className="hover:text-blue-600">
+          portfolio
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Header;
